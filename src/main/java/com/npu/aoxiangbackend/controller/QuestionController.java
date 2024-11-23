0f38841be32d1e2c6c7b0da051eeecd6b030b553
贾聪毅 <50274621@gitlab.com>
2024-11-24 01:03:38 +0800

    完成Question实体的增删改查服务

--- src/main/java/com/npu/aoxiangbackend/controller/QuestionController.java
+++ src/main/java/com/npu/aoxiangbackend/controller/QuestionController.java
@@ -7,7 +7,8 @@ import com.npu.aoxiangbackend.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/question")
+@RestController
+@RequestMapping("/api/question")
 public class QuestionController {
     private final QuestionService questionService;
 
